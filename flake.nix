32d4236f7d5d54fe5206e2c72c58ecbf3200620e
comavius <comavius@proton.me>
2025-01-21 00:49:04 +0900

    ✨ add flakes for openapi-generator-cli

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          # for openapitools/openapi-generator-cli
+          buildInputs = [ pkgs.jdk23 ];
+        };
+        formatter = pkgs.alejandra;
+      }
+    );
+}
