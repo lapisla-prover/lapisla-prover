f0bdfc6f18ecd06efffbcfbd07473632b7eecfbb
PonponJuice <ponjuice2021@gmail.com>
2025-01-24 14:06:55 +0900

    とりあえず

--- apps/backend/src/api/login.service.ts
+++ apps/backend/src/api/login.service.ts
@@ -11,16 +11,22 @@ export class LoginService {
         this.prisma = prismaService;
     }
 
-    public async callbackGitHubOAuth(code: string, state: string, ): Promise<null> {
-        throw new Error('Method not implemented.');
+    public async callbackGitHubOAuth(code: string, state: string, ): Promise<{session_id: string, url: string}> {
+        const session_state = "" // get_state_from_session();
+        if (state !== session_state) {
+            throw new Error('Invalid state');
+        }
+
+
+        return {session_id: "", url: ""};
     }
-    /**
-     * Login with GitHub
-     * 
-     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
-     * @param reportProgress flag to report request and response progress.
-     */
-    public async loginWithGitHub(): Promise<null> {
-        throw new Error('Method not implemented.');
+    
+    public async loginWithGitHub(): Promise<{state: string, url: string}> {
+        const state = Math.random().toString(36).slice(-8);
+        // TODO: save state to session
+
+        const url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${process.env.GITHUB_CLIENT_ID}&state=${state}`;
+
+        return {state, url};
     }
 }
--- apps/backend/src/controllers/login.controller.ts
+++ apps/backend/src/controllers/login.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { LoginService } from '../api/login.service';
+import { Response } from 'express';
 
 @Controller('login')
 export class LoginController {
     constructor(private loginService: LoginService) {}
 
     @Get()
-    async login() {
-        return await this.loginService.loginWithGitHub();
+    async login(@Res() res: Response) {
+        const {state, url} = await this.loginService.loginWithGitHub();
+        res.cookie('session_id', state, { httpOnly: true, maxAge: 600000 });
+        res.redirect(url);
+        return res.send(state);
     }
 
     @Get('callback')
-    async loginCallback(@Query('code') code: string, @Query('state') state: string) {
-        return await this.loginService.callbackGitHubOAuth(code, state);
+    async loginCallback(@Query('code') code: string, @Query('state') state: string, @Res() res: Response) {
+        const {session_id, url} = await this.loginService.callbackGitHubOAuth(code, state);
+        res.cookie('session_id', session_id, { httpOnly: true });
+        res.redirect(url);
+        return res.send(session_id);
     }
 }
