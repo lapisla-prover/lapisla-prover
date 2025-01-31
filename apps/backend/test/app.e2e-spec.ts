d53d73f52e505b36cdaaa9a41836be70207a0d33
abap34 <abap0002@gmail.com>
2025-01-31 12:58:00 +0900

    test(backend):  add mock app module e2e test

--- apps/backend/test/app.e2e-spec.ts
+++ apps/backend/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule, MockAppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +20,30 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+});
+
+
+describe('MockAppModule (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [MockAppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello from MockAuthService!');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
