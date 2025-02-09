532fa2f343d1263002218e1ae433d341aefcf43e
zer0-star <zer0star.65535@gmail.com>
2025-02-09 16:04:08 +0900

    feat(docs): add page using-lapisla

--- apps/docs/app/_meta.tsx
+++ apps/docs/app/_meta.tsx
@@ -3,6 +3,7 @@ import { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   index: "Introduction",
   "how-to-write-a-proof": "How to Write a Proof",
+  "using-lapisla": "Using Lapisla",
   reference: "Reference",
 };
 
